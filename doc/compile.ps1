e8ed3d8a63982b060375b9478261ce8c9077d1f6
JSchmiegel <jonas@ullr>
2022-01-20 22:16:50 +0100

    added info about the dataset in documentation

--- doc/compile.ps1
+++ doc/compile.ps1
@@ -13,10 +13,10 @@ function showNotification($time) {
 
 $stopwatch = New-Object System.Diagnostics.Stopwatch
 $stopwatch.Start()
-pdflatex -shell-escape ProjectPreRegister.tex
-# bibtex ProjectPreRegister
-# pdflatex -shell-escape ProjectPreRegister
-# pdflatex -shell-escape ProjectPreRegister
+pdflatex -shell-escape main
+bibtex main
+pdflatex -shell-escape main
+pdflatex -shell-escape main
 $stopwatch.Stop()
 $stopwatch
 
