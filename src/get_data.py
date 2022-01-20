98e7ded0599532e0cb3b680ac078a7d48f085e88
JSchmiegel <jonas@ullr>
2022-01-20 12:15:02 +0100

    added get_data.py to get df from dataset

---
+++ src/get_data.py
@@ -0,0 +1,22 @@
+from xmlrpc.client import boolean
+import numpy as np
+import pandas as pd
+
+import json
+from pathlib import Path
+
+def get_data(pre:boolean) -> pd.DataFrame:
+    if pre:
+        data_path = 'data/data_pre'
+    else:
+        data_path = 'data'
+    
+    pathlist = Path(data_path).glob('**/*.json')
+    df_sclices = []
+
+    for path in pathlist:
+        data = json.load(open(path))
+        df_sclices.append(pd.DataFrame(data['playlists']))
+
+    df = pd.concat(df_sclices)
+    return df
