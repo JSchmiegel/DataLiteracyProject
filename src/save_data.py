59f74c94d1717d8f893fab8927bbe9ac80b5f4a6
JSchmiegel <jonas@ullr>
2022-01-30 22:50:05 +0100

    added top popular as table

--- src/save_data.py
+++ src/save_data.py
@@ -16,8 +16,16 @@ def save_data(key, value):
     except FileNotFoundError:
         pass
 
+    value = edit_str(value)
     dict_var[key] = value
 
     with open(file_path, "w") as f:
         for key in dict_var.keys():
             f.write(f"{key};{dict_var[key]}\n")
+
+
+def edit_str(value) -> str:
+    if type(value) == str:
+        if len(value) > 25:
+            return f'{value[:22]}...'
+    return value
